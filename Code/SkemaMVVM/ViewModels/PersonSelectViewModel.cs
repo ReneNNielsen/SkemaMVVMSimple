78e729550f99e27673318ba4f1003fcfea0ea224
ReneNNielsen <renennielsen@gmail.com>
2015-11-16 14:30:54 +0100

    Teacher Get all added

--- Code/SkemaMVVM/ViewModels/PersonSelectViewModel.cs
+++ Code/SkemaMVVM/ViewModels/PersonSelectViewModel.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Services;
+using Models;
 
 namespace ViewModels
 {
     public class PersonSelectViewModel : BaseViewModel
     {
+        private PersonSelectViewData person;
         public PersonSelectViewModel()
         {
-            
         }
+
+        public PersonSelectViewData Person
+        {
+            get
+            {
+                return person;
+            }
+            protected set
+            {
+                person = value;
+            }
+        }
+
+
+
+        
     }
 }
--- Code/SkemaMVVM/ViewModels/TeacherSelectViewModel.cs
+++ Code/SkemaMVVM/ViewModels/TeacherSelectViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Models;
+using Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,8 +14,43 @@ namespace ViewModels
     {
         public TeacherSelectViewModel()
         {
-
+            Person = new TeacherSelectViewData();
         }
 
+        private void addPersonsToPersons()
+        {
+            TeacherContext tc = new TeacherContext();
+            List<Teacher> allTeachers = tc.GetAllTeachers();
+            foreach (Teacher teacher in allTeachers)
+            {
+                Person.Persons.Add(new PersonListItemViewData()
+                {
+                    Id = teacher.Id,
+                    FirstName = teacher.FirstName,
+                    LastName = teacher.LastName
+                });
+            }
+        }
+
+        public bool CanEdit
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public ActionCommand EditTeacherCommand
+        {
+            get
+            {
+                return new ActionCommand(p => EditTeacher(),  p => CanEdit);
+            }
+        }
+
+        private void EditTeacher()
+        {
+            
+        }
     }
 }
