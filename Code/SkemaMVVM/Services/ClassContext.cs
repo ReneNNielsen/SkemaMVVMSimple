bdea6a40f8dbee444df2057ce28da6aff16fb758
PingPongLee <djda9l@gmail.com>
2015-11-17 12:26:36 +0100

    Add more context classes

---
+++ Code/SkemaMVVM/Services/ClassContext.cs
@@ -0,0 +1,62 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ClassContext : DataContext
+    {
+        DataContext context;
+
+        public ClassContext()
+        {
+            context = new DataContext();
+        }
+
+        public DataContext DataContext
+        {
+            get { return context; }
+        }
+
+        public bool AddNewClass(Class newClass)
+        {
+            Class insertedClass = context.Classes.Add(newClass);
+            context.SaveChanges();
+            if (insertedClass.Id > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Class GetClass(int id)
+        {
+            return context.Classes.Find(id);
+        }
+
+        public Class GetClass(string name)
+        {
+            List<Class> allClasses = context.Classes.ToList();
+            foreach(Class theClass in allClasses)
+            {
+                if (theClass.Name == name)
+                    return theClass;
+            }
+            return null;
+        }
+
+        public List<Class> GetAllClasses()
+        {
+            List<Class> allClasses = context.Classes.ToList();
+            if (allClasses != null)
+            {
+                return allClasses;
+            } 
+            return null;                
+        }
+    }
+}
---
+++ Code/SkemaMVVM/Services/SubjectContext.cs
@@ -0,0 +1,62 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class SubjectContext : DataContext
+    {
+        DataContext context;
+
+        public SubjectContext()
+        {
+            context = new DataContext();
+        }
+
+        public DataContext DataContext
+        {
+            get { return context; }
+        }
+
+        public bool AddNewSubject(Subject newSubject)
+        {
+            Subject insertedSubject = context.Subjects.Add(newSubject);
+            context.SaveChanges();
+            if (insertedSubject.Id > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Subject GetSubject(int id)
+        {
+            return context.Subjects.Find(id);
+        }
+
+        public Subject GetSubject(string name)
+        {
+            List<Subject> allSubjects = context.Subjects.ToList();
+            foreach(Subject theSubject in allSubjects)
+            {
+                if (theSubject.Name == name)
+                    return theSubject;
+            }
+            return null;
+        }
+
+        public List<Subject> GetAllSubjects()
+        {
+            List<Subject> allSubjects = context.Subjects.ToList();
+            if (allSubjects != null)
+            {
+                return allSubjects;
+            } 
+            return null;                
+        }
+    }
+}
