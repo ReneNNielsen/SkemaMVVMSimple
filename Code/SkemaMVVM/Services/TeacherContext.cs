d93ddce4d46b1d37c3f067e8f3f22afd9e3c9ca1
PingPongLee <djda9l@gmail.com>
2015-11-19 10:35:55 +0100

    Updated TeacherContext to be able to update subjects

--- Code/SkemaMVVM/Services/TeacherContext.cs
+++ Code/SkemaMVVM/Services/TeacherContext.cs
@@ -1,4 +1,5 @@
 ﻿using Models;
+using Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -11,15 +12,26 @@ namespace Services
 {
     public class TeacherContext : DataContext
     {
+        /// <summary>
+        /// Constructor for TeacherContext.
+        /// </summary>
         public TeacherContext()
         {
         }
 
-        public DataContext DataContext
+        /// <summary>
+        /// Gets the inherited DataContext object.
+        /// </summary>
+        private DataContext DataContext
         {
             get { return Context; }
         }
 
+        /// <summary>
+        /// Adds a new teacher to the database.
+        /// </summary>
+        /// <param name="newTeacher">Teacher object containing the new teachers data.</param>
+        /// <returns>True if success.</returns>
         public bool AddNewTeacher(Teacher newTeacher)
         {
             Teacher insertedTeacher = Context.Teachers.Add(newTeacher);
@@ -32,10 +44,22 @@ namespace Services
             return false;
         }
 
+        /// <summary>
+        /// Edits an already created teacher based on the ID in the Teacher object provided.
+        /// </summary>
+        /// <param name="newTeacherData">Teacher object containing the new data.</param>
+        /// <returns>True if success.</returns>
         public bool EditTeacher(Teacher newTeacherData)
         {
             Teacher oldTeacherData = Context.Teachers.Find(newTeacherData.Id);
-            if(oldTeacherData != null)
+
+            oldTeacherData.Subjects.Clear();
+            foreach (Subject newSubject in newTeacherData.Subjects)
+            {
+                oldTeacherData.Subjects.Add(newSubject);
+            }            
+
+            if (oldTeacherData != null)
             {
                 Context.Entry(oldTeacherData).CurrentValues.SetValues(newTeacherData);                
                 if(Context.SaveChanges() > 0)
@@ -47,11 +71,21 @@ namespace Services
             return false;
         }
 
+        /// <summary>
+        /// Gets a Teacher object from an ID.
+        /// </summary>
+        /// <param name="id">int ID from the Database.</param>
+        /// <returns>Teacher object containing teacher data.</returns>
         public Teacher GetTeacher(int id)
         {
             return Context.Teachers.Find(id);
         }
 
+        /// <summary>
+        /// Gets a Teacher object from a string name.
+        /// </summary>
+        /// <param name="name">String name</param>
+        /// <returns>Teacher object containing teacher data.</returns>
         public Teacher GetTeacher(string name)
         {
             List<Teacher> allTeachers = Context.Teachers.ToList();
@@ -63,6 +97,10 @@ namespace Services
             return null;
         }
 
+        /// <summary>
+        /// Gets a list of all Teachers
+        /// </summary>
+        /// <returns>List<Teacher> object</returns>
         public List<Teacher> GetAllTeachers()
         {
             List<Teacher> allTeachers = Context.Teachers.ToList();
